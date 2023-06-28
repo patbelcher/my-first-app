7ee144c7f0bdb95cb4c7116eaab1ca7ca35778bd
Pat <patbelcher@yahoo.com>
2023-06-28 12:13:15 -0400

    Made first component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
+import beachBabe from './beachBabe.png'
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Greeting />
+        <img src={beachBabe} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm <code>src/App.js</code> getting hungry!!.
         </p>
         <a
           className="App-link"
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React @ Boca Code
         </a>
       </header>
     </div>
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,8 @@
+
+export default function Greeting() {
+    const name = "Beach Babe"
+    return <>
+            <h1>Hello {name}</h1>
+            <h2>We're glad you're here!</h2>
+        </>
+}
